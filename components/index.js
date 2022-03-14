f958d60615969a8e5aa8a9fb83cfbc37c2fe0641
Kuan Ting Pan <kuanthedev@gmail.com>
2022-03-14 15:29:13 +0800

    Add Modal component and state management
    setup Recoil to manage modal state across components

--- components/index.js
+++ components/index.js
@@ -1,2 +1,5 @@
 export { default as HeaderLink } from "./HeaderLink";
 export { default as Header } from "./Header";
+export { default as Sidebar } from "./Sidebar";
+export { default as Feed } from "./Feed";
+export { default as Modal } from "./Modal";
