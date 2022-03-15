9c35cfa7e47eea11032bb78892ec072b36d598d4
Kuan Ting Pan <kuanthedev@gmail.com>
2022-03-15 18:09:06 +0800

    Add delete post api route

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -1,1 +1,18 @@
-export default function handler(req, res) {}
+import { connectToDatabase } from "../../../util/mongodb";
+import { ObjectId } from "mongodb";
+
+export default async function handler(req, res) {
+  const { db } = await connectToDatabase();
+  const {
+    method,
+    query: { id },
+  } = req;
+  if (method === "DELETE") {
+    try {
+      db.collection("posts").deleteOne({ _id: new ObjectId(id) }); // _id field in mongoDB is a ObjectId class
+      res.status(200).json({ message: "Post deleted" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+}
