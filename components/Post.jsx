c3ece08c69fc955200d62dab2b431587951ba0e2
Kuan Ting Pan <kuanthedev@gmail.com>
2022-03-15 18:11:36 +0800

    Update Post component to have full functionality

--- components/Post.jsx
+++ components/Post.jsx
@@ -9,20 +9,34 @@ import ThumbUpOffAltRoundedIcon from "@mui/icons-material/ThumbUpOffAltRounded";
 import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 import ReplyRoundedIcon from "@mui/icons-material/ReplyRounded";
 import CommentOutlinedIcon from "@mui/icons-material/CommentOutlined";
+import { useSession } from "next-auth/react";
+import TimeAgo from "timeago-react";
 
 import { modalState, modalTypeState } from "../atoms/modalAtom";
-import { getPostState } from "../atoms/postAtom";
+import { getPostState, handlePostState } from "../atoms/postAtom";
 
 const truncate = (str, length, ending = "...see more") =>
   str.length > length ? str.substr(0, length - 1) + ending : str;
 
 const Post = ({ post, modalPost }) => {
+  const { data: session } = useSession();
   const [modalOpen, setModalOpen] = useRecoilState(modalState);
   const [modalType, setModalType] = useRecoilState(modalTypeState);
   const [postState, setPostState] = useRecoilState(getPostState);
-
   const [showFullInput, setShowFullInput] = useState(false);
+  const [liked, setLiked] = useState(false);
+  const [handlePost, setHandlePost] = useRecoilState(handlePostState);
 
+  const deletePost = async () => {
+    const response = await fetch(`/api/posts/${post._id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setHandlePost(true);
+    setModalOpen(false);
+  };
   return (
     <div
       className={clsx(
@@ -40,7 +54,10 @@ const Post = ({ post, modalPost }) => {
             {post.username}
           </h6>
           <p className="text-sm dark:text-white/75 opacity-80">{post.email}</p>
-          {/* Timeage stamp */}
+          <TimeAgo
+            datetime={post.createdAt}
+            className="text-xs dark:text-white/75 opacity-80"
+          />
         </div>
         {modalPost ? (
           <IconButton onClick={() => setModalOpen(false)}>
@@ -53,7 +70,7 @@ const Post = ({ post, modalPost }) => {
         )}
       </div>
       {post.input && (
-        <div className="px-2.5 break-all md:break-normal">
+        <div className="px-2.5 py-2 break-all md:break-word">
           {modalPost || showFullInput ? (
             <p onClick={() => setShowFullInput(false)}>{post.input}</p>
           ) : (
@@ -76,6 +93,40 @@ const Post = ({ post, modalPost }) => {
           }}
         />
       )}
+      <div className="flex justify-evenly items-center dark:border-t border-gray-600/80 mx-2.5 pt-2 text-black/60 dark:text-white/75">
+        {modalPost ? (
+          <button className="postButton">
+            <CommentOutlinedIcon />
+            <h4>Comment</h4>
+          </button>
+        ) : (
+          <button
+            className={clsx("postButton", liked && "text-blue-500")}
+            onClick={() => setLiked((prev) => !prev)}
+          >
+            {liked ? (
+              <ThumbUpOffAltRoundedIcon className="-scale-x-100" />
+            ) : (
+              <ThumbUpOffAltOutlinedIcon className="-scale-x-100" />
+            )}
+            <h4>Like</h4>
+          </button>
+        )}
+        {session?.user?.email === post.email ? (
+          <button
+            className="postButton focus:text-red-400"
+            onClick={deletePost}
+          >
+            <DeleteRoundedIcon />
+            <h4>Delete post</h4>
+          </button>
+        ) : (
+          <button className="postButton ">
+            <ReplyRoundedIcon className="-scale-x-100" />
+            <h4>Share</h4>
+          </button>
+        )}
+      </div>
     </div>
   );
 };
