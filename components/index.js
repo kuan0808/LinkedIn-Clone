6efa11a965d853a536353a01ba3a6ece19826ab3
Kuan Ting Pan <kuanthedev@gmail.com>
2022-03-15 21:26:29 +0800

    Add widget component and SSR the news api

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from "./Header";
 export { default as Sidebar } from "./Sidebar";
 export { default as Feed } from "./Feed";
 export { default as Modal } from "./Modal";
+export { default as Widgets } from "./Widgets";
